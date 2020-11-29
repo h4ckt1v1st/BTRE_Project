aca5dfbe143cd054c8d015086b45a19673792550
Abhishek Singh <abhishek.viki@gmail.com>
2020-11-29 19:48:44 +0530

    Message Alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
+from django.contrib import messages
 from django.shortcuts import redirect, render
 
 
 def register(request):
     if request.method == 'POST':
-        # Register User
-        return
+        messages.error(request, 'Testing Error Message.')
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
